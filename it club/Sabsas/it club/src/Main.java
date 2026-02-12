c4c65398dd02ac0ff5fd656182dbd72847820496
glebapchili-lang <gleb.ap.chili@gmail.com>
2026-02-12 16:24:04 +0300

    Add files via upload

---
+++ it club/Sabsas/it club/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    int x = 100;
+        System.out.println(x);
+        System.out.println("Hello world");
+    int z = 582;
+    int y = 7850;
+    int sum = z+y;
+        System.out.println("sum");
+        System.out.println(z+y);
+        System.out.println("sum");
+        System.out.println(y+x);
+        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello" + name);
+
+    }
+}
--- it club/src/Main.java
+++ it club/src/Main.java
@@ -1,21 +1,29 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-    int x = 100;
-        System.out.println(x);
-        System.out.println("Hello world");
-    int z = 582;
-    int y = 7850;
-    int sum = z+y;
-        System.out.println("sum");
-        System.out.println(z+y);
-        System.out.println("sum");
-        System.out.println(y+x);
-        System.out.println("Hello world");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Hello" + name);
-
+    double R = 5;
+    double volume = (4/3) * Math.PI * (R * R * R);
+        System.out.println("ОбЪем = "  + volume);
+   double x = 6;
+   double y = 10;
+   double a = 1 + y;
+   double b =x * x - 4;
+   double c =1 / b;
+   double d = y + c;
+   double e = (x+y) / d;
+   double f = 2 * x;
+   double z = y * y;
+   double v = f + z- e;
+   double result = a * v;
+        System.out.println("Ответ ="+ result);
+   double p = 8;
+   double g = 4;
+   double h = 15;
+   double n = p + 2;
+   double l = g * h;
+   double j = g + l;
+   double t = n * g;
+   double u = t - h;
+   double cat = u / j;
+        System.out.println("Ответ=" + cat);
     }
 }
